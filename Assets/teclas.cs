4f60cc8730d410a1fca6958302fd13081b598ee2
lolagou <lolaemma2007@gmail.com>
2024-07-01 23:44:52 -0300

    scriptlisto

---
+++ Assets/teclas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teclas : MonoBehaviour
+{
+    public GameObject[] objetos;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            objetos[counter].SetActive(false);
+            counter--;
+            if (counter == -1)
+            {
+                counter = objetos.Length - 1;
+            }
+            objetos[counter].SetActive(true);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            objetos[counter].SetActive(false);
+            counter++;
+            if (counter == objetos.Length)
+            {
+                counter = 0;
+            }
+            objetos[counter].SetActive(true);
+        }
+    }
+}
